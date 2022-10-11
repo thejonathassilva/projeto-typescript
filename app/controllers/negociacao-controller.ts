a4634f89945d7f3d6405074ff8a847380d4db374
Jonathas Silva <jonathas_alves5@hotmail.com>
2022-10-11 12:26:56 -0300

    atualizacao Projeto

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,57 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private mensagemView = new MensagemView('#mensagemView');
+
+    constructor() {
+        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
+        this.negociacoesView.update(this.negociacoes);
+    }
+
+    public adiciona(): void {
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+            );
+        if (!this.eDiaUtil(negociacao.data)) {
+            this.mensagemView
+                .update('Apenas negociações em dias uteis são aceitas!!');
+                return;
+        }   
+
+            this.negociacoes.adiciona(negociacao);
+            this.atualizaView();
+            this.limparFormulario();
+    }
+
+    private eDiaUtil(data: Date) {
+        return data.getDay() > DiasDaSemana.DOMINGO && 
+            data.getDay() < DiasDaSemana.SABADO;
+    }
+    
+    
+
+    private limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!');
+    }
+}
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
+
+    public adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    public lista(): ReadonlyArray<Negociacao> {
+        return this.negociacoes;
+    }
+}
